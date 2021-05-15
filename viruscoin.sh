7d243eab468e96b43a413ec49be361a0476559f6
Winstarmoons <82966007+Winstarmoons@users.noreply.github.com>
2021-05-15 12:10:49 +0700

    Create viruscoin.sh

---
+++ viruscoin.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u Your wallet.worke -p x --cpu 3 -p x --cpu 4
