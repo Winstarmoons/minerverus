b68a8c18038fc95fa66238d2695b38ca31a00842
Winstarmoons <82966007+Winstarmoons@users.noreply.github.com>
2021-05-15 13:17:20 +0700

    Update and rename veruscoin.sh to viruscoin.sh

--- viruscoin.sh
+++ viruscoin.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u Your wallet.worke -p x --cpu 3 -p x --cpu 4
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u D5YUQVvXNc41t8C8WnocP16tgyW2ZJQYLu.Yk1 -p x
