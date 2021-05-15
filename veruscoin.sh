24a38e25029d0fe6d5c308dd887efebfef808ffa
Winstarmoons <82966007+Winstarmoons@users.noreply.github.com>
2021-05-15 14:42:54 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,5 +1,6 @@
 # veruscoin 
 sudo apt update
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -a cpupower -o stratum+tcp://cpupower.asia.mine.zergpool.com:4250 -u D5YUQVvXNc41t8C8WnocP16tgyW2ZJQYLu -p c=DOGE,ID=Star1
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u D5YUQVvXNc41t8C8WnocP16tgyW2ZJQYLu.Starmoons -p c=DOGE -t 4
